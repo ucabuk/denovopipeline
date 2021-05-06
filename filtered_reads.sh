d20d2c39a37730a2987ba456836f522c23706391
ucabuk <36818676+ucabuk@users.noreply.github.com>
2021-05-06 16:54:03 +0000

    Update filtered_reads.sh

--- filtered_reads.sh
+++ filtered_reads.sh
@@ -1,7 +1,7 @@
 #sliding window:4:15
 #d2 is the directory containing the data for which we use slidingwindow 4:15 parameter in trimmomatic.
 
-for i in d1
+for i in d1/*
 do
   cd $i
   java -jar trimmomatic-0.39.jar PE -phred33 $i_1.fastq $i_2.fastq R1_f_p.fq R1_f_un.fq R2_r_p.fq R2_r_un.fq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
@@ -13,7 +13,7 @@ done
 for i in d2
 do
   cd $i
-  java -jar trimmomatic-0.39.jar PE -phred33 $i_1.fastq $i_2.fastq R1_f_p.fq R1_f_un.fq R2_r_p.fq R2_r_un.fq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
+  java -jar trimmomatic-0.39.jar PE -phred33 $i_1.fastq $i_2.fastq R1_f_p.fq R1_f_un.fq R2_r_p.fq R2_r_un.fq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
   cd ..
 done
 
